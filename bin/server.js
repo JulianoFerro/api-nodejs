a3baf887ce3400b9bad5f025b6d17633c4a30d5e
Juliano <jferro1993@gmail.com>
2018-08-25 20:19:31 -0300

    Criado servidor

---
+++ bin/server.js
@@ -0,0 +1,67 @@
+
+const app = require('../src/app')
+const debug = require('debug')('api-nodejs:server');
+const http = require('http');
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+console.log('Rodando...' + port);
+
+server.listen(port);
+
+// Função retirada do gerador de código express
+function normalizePort(val) {
+	const port = parseInt(val, 10);
+	
+	if(isNaN(port)) {
+		return val;
+	}
+	if (port >= 0 ) {
+		return port;
+	}
+	return false;
+}
+
+function onError(error) {
+  if (error.syncall !== 'listen') {
+  	throw error;
+  }
+
+  const bind = typeof port === 'string'
+  	? 'Pipe' + port 
+  	: 'Port' + port;
+
+  	switch (error.code) {
+  		case 'EACCES':
+  			console.error(bind + ' requires elevated privileges');
+  			process.exit(1);
+  			break;
+  		case 'EADDRINUSE':
+  			console.error(bind + ' is already in use');
+  			process.exit(1);
+  			break;
+  		default:
+  			throw error;
+  	}
+
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+function onListening() {
+	const addr = server.address();
+	const bind = typeof addr === 'string'
+		? 'pipe ' + addr
+		: 'portt ' + addr.port;
+	debug('Listening on ' + bind);
+}
+
---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+
+app.use('/', route);
+
+module.exports = app;
